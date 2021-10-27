d2ff6112707ccf73f9ad809b38cbbd81b7f1b9a5
JASON-6700K\jandersen <JGAndersen@gmail.com>
2021-10-26 20:01:13 -0400

    import: ms pacman color, palette, and sound ROM

--- merlin32/mspac/rom.s
+++ merlin32/mspac/rom.s
@@ -8,11 +8,25 @@
 
 ;-------------------------------------------------------------------------------
 
-rom_5e ent             ; 8x8 Tile ROM
+tile_rom ent             ; 8x8 Tile ROM
 	putbin data\rom\5e
 
-rom_5f ent  	  	   ; 16x16 Sprite ROM
+sprite_rom ent  	  	   ; 16x16 Sprite ROM
     putbin data\rom\5f
+
+color_rom ent
+	putbin data\rom\82s123.7f
+
+palette_rom ent
+	putbin data\rom\82s126.4a
+
+sound_rom1 ent
+	putbin data\rom\82s126.1m
+
+sound_rom2 ent
+	putbin data\rom\82s126.3m
+
+
 
 
 
