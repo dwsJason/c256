4e8efd4ac31c74c4d0314773b542a7028ec4fe49
JASON-6700K\jandersen <JGAndersen@gmail.com>
2022-12-04 16:05:35 -0500

    Preserve cursor position when printing out the jiffy times

--- merlin32/jmixer/jmixer.s
+++ merlin32/jmixer/jmixer.s
@@ -193,6 +193,11 @@ main_loop mx %00
 ]lp
 		jsr WaitVBL
 		
+		ldx |CURSORX
+		ldy |CURSORY
+		phy
+		phx 	
+
 		ldx #96
 		ldy #0
 		jsl LOCATE	    ; cursor to top left of the screen
@@ -206,6 +211,11 @@ main_loop mx %00
 
 		lda <dpAudioJiffy
 		jsr myPRINTAH
+
+		plx
+		ply
+		jsl LOCATE
+
 
 		bra ]lp
 
@@ -468,4 +478,64 @@ myPUTS  mx %00
         rts
 
 ;------------------------------------------------------------------------------
+	do 0
+TrackSampler mx %00
 
+		; version 1
+		lda >pSample,x     ; 6  	; 43
+		asl 			   ; 2
+		and #$1FF   	   ; 3
+		sta <SampleData    ; 4
+
+		lda <pSample	   ; 4
+		adc <iRate  	   ; 4
+		sta <pSample	   ; 4
+		lda <pSample+2     ; 4 
+		adc <iRate+2	   ; 4
+		sta <pSample+2     ; 4
+		ldx <pSample+1     ; 4
+
+		; version 2 	   ; 32 clocks
+		ldx |offset,y      ; 5
+		lda >pSample,x     ; 6
+		asl 			   ; 2
+		and #$1FF   	   ; 3
+		sta |output,y      ; 5
+
+		iny 	  ;2
+		iny 	  ;2
+		cpy #512  ;3
+		bne ]lp   ;4 
+
+		; version 3
+		ldy #offset 	  ; 3  ; 18
+		lda [pSample],y   ; 7
+		asl 			  ; 2
+		and #$1FF   	  ; 3
+		sta <output,x     ; 5
+	fin
+
+
+
+;------------------------------------------------------------------------------
+;
+;         Table 2: Summary of MIDI Note Numbers for Different Octaves
+; (adapted from "MIDI by the Numbers" by D. Valenti - Electronic Musician 2/88)
+;
+;
+;Octave||                     Note Numbers
+;   #  ||
+;      || C   | C#  | D   | D#  | E   | F   | F#  | G   | G#  | A   | A#  | B
+;------------------------------------------------------------------------------
+;   0  ||   0 |   1 |   2 |   3 |   4 |   5 |   6 |   7 |   8 |   9 |  10 |  11
+;   1  ||  12 |  13 |  14 |  15 |  16 |  17 |  18 |  19 |  20 |  21 |  22 |  23
+;   2  ||  24 |  25 |  26 |  27 |  28 |  29 |  30 |  31 |  32 |  33 |  34 |  35
+;   3  ||  36 |  37 |  38 |  39 |  40 |  41 |  42 |  43 |  44 |  45 |  46 |  47
+;   4  ||  48 |  49 |  50 |  51 |  52 |  53 |  54 |  55 |  56 |  57 |  58 |  59
+;   5  ||  60 |  61 |  62 |  63 |  64 |  65 |  66 |  67 |  68 |  69 |  70 |  71
+;   6  ||  72 |  73 |  74 |  75 |  76 |  77 |  78 |  79 |  80 |  81 |  82 |  83
+;   7  ||  84 |  85 |  86 |  87 |  88 |  89 |  90 |  91 |  92 |  93 |  94 |  95
+;   8  ||  96 |  97 |  98 |  99 | 100 | 101 | 102 | 103 | 104 | 105 | 106 | 107
+;   9  || 108 | 109 | 110 | 111 | 112 | 113 | 114 | 115 | 116 | 117 | 118 | 119
+;  10  || 120 | 121 | 122 | 123 | 124 | 125 | 126 | 127 |
+;
