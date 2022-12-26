422262188458e3bb2f4bba430f0dc7d0879a9e9e
JASON-6700K\jandersen <JGAndersen@gmail.com>
2022-12-25 20:35:56 -0500

    import: missing mixer files

---
+++ merlin32/jmixer/axelf.s
@@ -0,0 +1,12 @@
+;
+; Midi song
+;
+		rel
+		lnk axelf.l
+
+;------------------------------------------------------------------------------
+
+midi_axelf ent
+	putbin data\axel-f.mid
+
+
---
+++ merlin32/jmixer/insts.s
@@ -0,0 +1,12 @@
+;
+; Midi Instruments
+;
+		rel
+		lnk insts.l
+
+;------------------------------------------------------------------------------
+
+inst_violin ent
+	putbin data\arcoviolingx2.s3i
+
+
