ec4e97379e4e185c3507d48e38a3a8e5e5b5c6e0
JASON-6700K\jandersen <JGAndersen@gmail.com>
2022-12-31 12:23:02 -0500

    mspac: add the mappy commands, so that I have source level debugging

--- merlin32/mspac/build.bat
+++ merlin32/mspac/build.bat
@@ -1,5 +1,7 @@
-..\bin\Merlin32.exe -V ..\bin\Library link.s 
+..\bin\Merlin32.exe -V ..\bin\Library link.s
+..\bin\mappy.exe mspacman.s16_S01_Main_Output.txt $002100 > mspacman.lst
 ..\bin\omf2hex mspacman.s16 mspacman.hex
+
 run256 mspacman.hex
 
 
